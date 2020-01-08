c538d85d212f9f62fb589ea73369bf20843d93ab
Adam Schaefers <paxchristi888@gmail.com>
2020-01-08 10:14:49 -0800

    update template

--- files/shutdown.el
+++ files/shutdown.el
@@ -1,9 +1,18 @@
-#!/bin/setsid /bin/dash
-":"; exec /boot/emacs/bin/emacs --quick --script "$0" "$@" </dev/tty1 >/dev/tty1 2>&1 # -*- mode: emacs-lisp; lexical-binding: t; -*-
+#!/bin/setsid /boot/bin/dash
+":"; LD_LIBRARY_PATH=/boot/emacs exec /boot/bin/emacs --quick --script "$0" "$@" </dev/tty1 >/dev/tty1 2>&1 # -*- mode: emacs-lisp; lexical-binding: t; -*-
 
-(setenv "PATH" "/bin")
-(setenv "SHELL" "/bin/dash")
+(setq debug-on-error nil)
 
-(setq exec-path '("/bin")
-      shell-file-name "/bin/dash"
-      debug-on-error nil)
+(setenv "PATH" "/boot/bin:/bin")
+(setq exec-path '("/boot/bin" "/bin"))
+
+(setenv "SHELL" "/boot/bin/dash")
+(setq shell-file-name "/boot/bin/dash")
+
+(require 'subr-x)
+
+(defun process-exit-code-and-output (program &rest args)
+  "Run PROGRAM with ARGS and return the exit code and output in a list."
+  (with-temp-buffer
+    (list (apply 'call-process program nil (current-buffer) nil args)
+          (buffer-string))))
