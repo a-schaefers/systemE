fd01995f3acf7997df24fcfc0a6a415de99cbeab
Adam Schaefers <paxchristi888@gmail.com>
2020-01-10 13:41:32 -0800

    move kiss pkg build files ../

---
+++ sinit/files/config.h
@@ -0,0 +1,3 @@
+static char *const rcinitcmd[]     = { "/lib/init/rc.boot.el", NULL };
+static char *const rcrebootcmd[]   = { "/lib/init/rc.shutdown.el", "reboot", NULL };
+static char *const rcpoweroffcmd[] = { "/lib/init/rc.shutdown.el", "poweroff", NULL };
